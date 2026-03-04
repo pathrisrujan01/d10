a919f354f74d1098b9b7336c7a1ac1a59b8f7837
pathrisrujan01 <srujanpathri24@gmail.com>
2026-03-04 12:52:29 +0530

    if condition

---
+++ if condition/div by 7.py
@@ -0,0 +1,5 @@
+num=78
+if(num%7==0):
+    print(num," is div by 7")
+else:
+    print(num," is not div by 7") 
---
+++ if condition/greatest.py
@@ -0,0 +1,21 @@
+# num1=81
+# num2=99
+# num3=100
+# if(num1>num2 and num1>num3):
+#     print("greater number is:",num1)
+# elif(num2>num1 and num2>num3):
+#     print("greater number is " ,num2)
+# else:
+#     print("the greater number is " ,num3)
+
+#or input method
+
+num1=int(input("enter the num1: "))
+num2=int(input("enter the num2: "))
+num3=int(input("enter the num3: "))
+if(num1>num2 and num1>num3):
+    print("greater number is:",num1)
+elif(num2>num1 and num2>num3):
+    print("greater number is " ,num2)
+else:
+    print("the greater number is " ,num3)
